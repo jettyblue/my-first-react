6ef330d686d5176bf86162ca422f448e0b5aa030
Casey Harding <casey.r.harding@gmail.com>
2021-11-11 18:20:35 -0500

    React Router in place.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +19,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/**
+ * location -> where you are
+ * history -> where you've been (or will go)
+ * match -> info about the url (url params)
+ */
