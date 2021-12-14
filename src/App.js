9fe7282da3e52e667b100f9d0c40b97ba0688381
unknown <lindsay.kelly15@gmail.com>
2021-12-14 18:45:43 -0500

    MemberForm added to app, forms available but not functioning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import MemberForm from './Components/MemberForm';
+import Member from './Components/Member';
+import { Route, Link } from 'react-router-dom';
+
+
+// the shape of the state that drives the form
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: ''
+}
 
 function App() {
+  const [members, setMembers] = useState([])
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [error, setError] = useState('');
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({...formValues, [inputName]: inputValue});
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      memberName: formValues.memberName.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+
+    if(!newMember.memberName || !newMember.email || !newMember.role) {
+      setError('All fields are required');
+    }else {
+      axios.post('https://fakeapi.com', newMember)
+        .then(res => {
+          // console.log(res)
+          const memberFromServer = res.data;
+          setMembers([ memberFromServer, ...members ]); // get friendFromServer as well as all the initial friends
+          setFormValues(initialFormValues);
+        }).catch(err => console.error(err))
+        .finally(() => setError(''))
+    }
+  }
+
+  useEffect(() => {
+    axios.get('https://fakeapi.com').then(res => setMembers(res.data))
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{error}</h2>
+
+      <MemberForm 
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+      />
+
+      {
+        members.map(member => {
+          return (
+            <Member key={member.id} details={member} />
+          )
+        })
+      }
     </div>
   );
 }
---
+++ src/Components/MemberForm.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+export default function MemberForm(props) {
+
+    const { values, update, submit } = props
+  
+    const onChange = evt => {      
+      // console.log(evt.target.name)
+      const name = evt.target.name;
+      const { value } = evt.target;
+      // const { name, value } = evt.target;  -- same as above consts
+  
+      update(name, value);
+    }
+  
+    const onSubmit = evt => {
+      evt.preventDefault();
+      submit();
+    }
+  
+    return (
+      <form className='form container' onSubmit={onSubmit}>
+        <div className='form-group inputs'>
+
+          <label>Member Name
+                <input
+                  name='name'  // this name has to be exactly the same as label name
+                  type='text'
+                  placeholder='Type your member name here'
+                  maxLength='50'
+                  value={values.memberName}
+                  onChange={onChange}
+                />
+          </label>
+  
+          <label>Email
+                <input
+                  name='email'  // this name has to be exactly the same as label name
+                  type='email'
+                  placeholder='Type your email here'
+                  value={values.email}
+                  onChange={onChange}
+                />
+          </label>
+
+          <label>Role
+                <select value={values.role} name='role' onChange={onChange}>
+                  <option value=''>-- Select a Role --</option>
+                  <option value='Newbie'>New Member</option>
+                  <option value='Current'>Current Member</option>
+                  <option value='Past'>Previous Member</option>
+                </select>
+          </label>
+  
+          <div className='submit'>
+            <button>submit</button>
+          </div>
+        </div>
+      </form>
+    )
+  }
